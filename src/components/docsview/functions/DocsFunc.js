fe7722d45e30dccc4149b09f313807d5dceff262
Kariaro <25615845+Kariaro@users.noreply.github.com>
2021-09-07 10:53:17 +0200

    style: Made the userdata easer to read

--- src/components/docsview/functions/DocsFunc.js
+++ src/components/docsview/functions/DocsFunc.js
@@ -37,6 +37,26 @@ function StringifyParam(param, allowName = true) {
 	);
 }
 
+function StringifyUserListParam(params) {
+	if(Object.keys(params).length === 0) return '';
+
+	let result = [];
+	let items = Object.keys(params);
+	for(let i = 1; i < items.length; i++) {
+		let param = params[items[i]];
+
+		if(param.name) {
+			result.push(param.name);
+		} else if(param.type.length === 1) {
+			result.push(param.type[0]);
+		} else {
+			result.push('[' + param.type + '] ');
+		}
+	}
+
+	return result.join(', ');
+}
+
 const MARKDOWN_LINK_TARGET = (href, children, title) => {
 	return href.startsWith('#') ? '':'_blank';
 };
@@ -82,7 +102,7 @@ function DocsFunc(namespace_name, function_name, json, local) {
 			<dl>
 				<dt className={`${styles.DeclHeader} ${local ? styles.Userdata:styles.Tabledata}`}>
 					<span className={`${styles.DeclSeparator}`}><code className={`${styles.DeclSandbox}`}>{json.sandbox}</code></span>
-					<span className={`${styles.DeclSeparator}`}><code className={`${styles.DeclName}`}>{local_id}</code></span>
+					<span className={`${styles.DeclSeparator}`}><code className={`${styles.DeclName}`}>{local ? function_name:local_id}</code></span>
 					<span className={`${styles.DeclSeparator}`}><span className={`${styles.DeclParameters}`}>{DeclParameters(json.params)}</span></span>
 					<CopyToClipboard text={window.location.href.replace(new RegExp(window.location.hash, 'g'), '') + '#' + link_id} onCopy={() => {
 						window.location.hash = '#'
@@ -92,6 +112,16 @@ function DocsFunc(namespace_name, function_name, json, local) {
 				<dd>
 					<p className={`${styles.DeclDescription}`}>
 						<ReactMarkdown linkTarget={MARKDOWN_LINK_TARGET}>{json.description}</ReactMarkdown>
+
+						{local ? from_func(() => {
+							let ns_name = namespace_name.substr(namespace_name.lastIndexOf('.') + 1);
+							return (
+								<div>
+									<p>Example Usage:</p>
+									<pre>variable_{ns_name}:{function_name}({StringifyUserListParam(json.params)});</pre>
+								</div>
+							);
+						}):''}
 					</p>
 					<table className={`${styles.DeclTables}`}>
 						<tbody>
